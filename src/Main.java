71e098b2f2a554b924cc917db63886703e91bf31
Mariscos1 <ramon.marquez3@yahoo.com>
2019-12-09 17:57:42 -0600

    Your gay

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    //Calls all the important things yes it does
+    //you gay
+    //how are yoiu
+
+    public static void main(String[] args) {
+        Notepad notepad = new Notepad();
+    }
+}
