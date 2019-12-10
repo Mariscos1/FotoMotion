f2c454ec4ebe2dc8b21bd8b272aff9c70807845c
Mariscos1 <ramon.marquez3@yahoo.com>
2019-12-09 18:13:13 -0600

    Yay

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     //Calls all the important things yes it does
     //you gay
     //how are yoiu
-
+    //This better be good
     public static void main(String[] args) {
         Notepad notepad = new Notepad();
     }
